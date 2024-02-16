8943699421c4d9182c0c39d49c53f0d19f2b37a3
Gabriel Leal <gabryellows@gmail.com>
2024-02-16 20:11:44 -0300

    ajuste nos nome dos caminhos e arquivos

--- node-app/app.js
+++ node-app/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const porta = 8080
-const connection = require('./database/Connection')
+const connection = require('./db/Connection')
 
 app.get('/hello', async (req, res) => {
     res.sendStatus(200)
