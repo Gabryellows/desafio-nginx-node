3c0e3bc775348a8cfe60b8ef905d85eb06f297b2
Gabriel Leal <gabryellows@gmail.com>
2024-02-16 20:05:46 -0300

    desafio nginx proxy reverso com node

---
+++ README.md
@@ -0,0 +1,6 @@
+# desafio_nginx_node
+
+## Links imagens Docker Hub
+
+* Nginx: https://hub.docker.com/r/gabryellows/nginx
+* aplicão node: https://hub.docker.com/r/gabryellows/app
---
+++ node-app/db/Connection.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql')
+
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+}
+
+const connection = mysql.createConnection(config)
+connection.connect((error) => {
+    if (error) {
+       console.error('Erro ao conectar ao MySQL: ' + error);
+       return;
+    }
+    console.log('Conexão bem sucedida ao MySQL.');
+});
+
+connection.query(`CREATE TABLE IF NOT EXISTS people (nome varchar(255))`)
+connection.query(`INSERT INTO people values ('Ganriel'),('Vini'),('Lari')`)
+
+module.exports = connection
